164b27073e569fdfd68312dcfa66b2b3296c8844
BlackUserSide <nesmserg@gmail.com>
2020-05-28 19:58:08 +0300

    Full verstka

--- views/index.tpl.php
+++ views/index.tpl.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,18 +10,23 @@
     <link rel="stylesheet" href="/css/main.css.map">
     <title><?php echo $pageData['title'] ?></title>
 </head>
+
 <body>
     <header class="site-header">
+        <div class="bg-lock"></div>
         <div class="container">
             <div class="row">
                 <div class="col-xl-7 col-md-7 col-sm-7 col-lg-7 col-xs-7">
-                   <div class="main-composition">
-                       <h1 class="h1">Накрутка в инстаграмме</h1>
-                       <p class="main-text">Быстро | Качественно | Надежно</p>
-
-                   </div>
+                    <div class="main-composition">
+                        <h1 class="h1">Накрутка в инстаграмме</h1>
+                        <p class="main-text">Быстро | Качественно | Надежно</p>
+                        <i class="fas fa-chevron-down"></i><br>
+                        <i class="fas fa-chevron-down"></i><br>
+                        <i class="fas fa-chevron-down"></i><br>
+                    </div>
                 </div>
                 <div class="col-xl-5 col-md-5 col-sm-5 col-lg-5 col-xs-5">
+                    <p class="logo-nav">Выбери услугу</p>
                     <nav class="main-nav">
                         <ul>
                             <li><a href="#">Лайки</a></li>
@@ -34,7 +40,116 @@
             </div>
         </div>
     </header>
+    <section class="main-parse">
+        <div class="liks-category category-wrapper">
+            <div class="top-line-category">
+                <h1 class="h1">Лайки</h1>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-4 col-sm-12 col-lg-4 col-xl-4">
+                        <div class="item-wrapper">
+                            <div class="icon-wrap">
+                                <i class="fas fa-heart"></i>
+                            </div>
+                            
+                            <h3 class="name-service">1000 лайков</h3>
+                            <p class="old-price">Старая цена: <span>250грн</span></p>
+                            <p class="price">150 грн.</p>
+                            <a href="#" class="start-order">Заказать</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sums-category category-wrapper">
+            <div class="top-line-category">
+                <h1 class="h1">Подписчики</h1>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-4 col-sm-12 col-lg-4 col-xl-4">
+                        <div class="item-wrapper">
+                            <div class="icon-wrap">
+                                <i class="fas fa-user-friends"></i>
+                            </div>
+                            
+                            <h3 class="name-service">1000 лайков</h3>
+                            <p class="old-price">Старая цена: <span>250грн</span></p>
+                            <p class="price">150 грн.</p>
+                            <a href="#" class="start-order">Заказать</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sums-category category-wrapper">
+            <div class="top-line-category">
+                <h1 class="h1">Статистика</h1>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-4 col-sm-12 col-lg-4 col-xl-4">
+                        <div class="item-wrapper">
+                            <div class="icon-wrap">
+                                <i class="fas fa-user-cog"></i>
+                            </div>
+                            
+                            <h3 class="name-service">1000 лайков</h3>
+                            <p class="old-price">Старая цена: <span>250грн</span></p>
+                            <p class="price">150 грн.</p>
+                            <a href="#" class="start-order">Заказать</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sums-category category-wrapper">
+            <div class="top-line-category">
+                <h1 class="h1">Просмотры</h1>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-4 col-sm-12 col-lg-4 col-xl-4">
+                        <div class="item-wrapper">
+                            <div class="icon-wrap">
+                                <i class="fas fa-user-cog"></i>
+                            </div>
+                            
+                            <h3 class="name-service">1000 просмотров</h3>
+                            <p class="old-price">Старая цена: <span>250грн</span></p>
+                            <p class="price">150 грн.</p>
+                            <a href="#" class="start-order">Заказать</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="sums-category category-wrapper">
+            <div class="top-line-category">
+                <h1 class="h1">Автолайки</h1>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12 col-md-4 col-sm-12 col-lg-4 col-xl-4">
+                        <div class="item-wrapper">
+                            <div class="icon-wrap">
+                                <i class="fas fa-user-cog"></i>
+                            </div>
+                            
+                            <h3 class="name-service">1000 просмотров</h3>
+                            <p class="old-price">Старая цена: <span>250грн</span></p>
+                            <p class="price">150 грн.</p>
+                            <a href="#" class="start-order">Заказать</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </section>
     <script src="/js/jquery.min.js"></script>
-    
+
 </body>
+
 </html>
