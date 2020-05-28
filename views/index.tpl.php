6a6a8d1f8cbc1134f07dee476fdfbe75b310b889
BlackUserSide <nesmserg@gmail.com>
2020-05-28 18:45:55 +0300

    reborn 2

--- views/index.tpl.php
+++ views/index.tpl.php
@@ -11,16 +11,16 @@
 </head>
 <body>
     <header class="site-header">
-        <div class="container-fluid">
+        <div class="container">
             <div class="row">
-                <div class="col-xl-6 col-md-6 col-sm-6 col-lg-6 col-xs-6">
+                <div class="col-xl-7 col-md-7 col-sm-7 col-lg-7 col-xs-7">
                    <div class="main-composition">
                        <h1 class="h1">Накрутка в инстаграмме</h1>
-                       <p>Быстро | Качественно|Надежно</p>
+                       <p class="main-text">Быстро | Качественно | Надежно</p>
 
                    </div>
                 </div>
-                <div class="col-xl-6 col-md-6 col-sm-6 col-lg-6 col-xs-6">
+                <div class="col-xl-5 col-md-5 col-sm-5 col-lg-5 col-xs-5">
                     <nav class="main-nav">
                         <ul>
                             <li><a href="#">Лайки</a></li>
